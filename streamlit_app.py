adea2916d66f89b6f61d5b689917f7092c81317b
janakasandun <janaka24online@gmail.com>
2022-12-24 12:01:43 -0800

    Update streamlit_app.py
    Added a headed
    Added few text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberrey Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
